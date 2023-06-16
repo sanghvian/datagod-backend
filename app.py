d071e454eb674d9aea7f2f46a333c1523a70dfa9
Ankit Sanghvi <ankit.sanghavi87@gmail.com>
2023-06-16 12:56:32 +0530

    functional rag model

--- app.py
+++ app.py
@@ -3,12 +3,8 @@ import openai
 import os
 import json
 from flask_cors import CORS
-from scripts.get_cloud_architecture import get_cloud_architecture
-from scripts.get_system_diagram import return_infra_code,generate_diagram_image_and_upload_to_s3
-from scripts.get_cloud_iac import return_iac_code
-from scripts.generate_questions import return_questions
-from scripts.iterate_cloud_architecture import iterate_cloudplan
-from scripts.get_cloud_cost import calculate_cost_cloudplan
+from scripts.get_rag_answer import get_rag_answer
+from scripts.openai_complete import openai_complete
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -27,8 +23,12 @@ OPENAI_API_KEY = os.environ.get("OPENAI_API_KEY")
 openai.api_key = OPENAI_API_KEY
 
 
-@app.route('/get-cloud-architecture', methods=['POST'])
-def cloud_archi_generate():
+@app.route('/hello', methods=['GET'])
+def hello():
+    return 'active', 200
+
+@app.route('/chat', methods=['POST'])
+def get_ans_and_docs():
     data = request.get_json()
     prompt = data.get('prompt')
 
@@ -36,48 +36,10 @@ def cloud_archi_generate():
         return jsonify({'error': 'Statement is missing'}), 400
 
     # Return the query result as JSON
-    result = get_cloud_architecture(prompt)
+    result = get_rag_answer(prompt)
     return jsonify(result), 200
 
-@app.route('/get-system-diagram', methods=['POST'])
-def system_diagram_generate():
-    data = request.get_json()
-    prompt = data.get('prompt')
-
-    if not prompt:
-        return jsonify({'error': 'Statement is missing'}), 400
-
-    # Return the query result as JSON
-    result = return_infra_code(prompt)
-    img_url = generate_diagram_image_and_upload_to_s3(result, "cloudpilot-systems-design-diagrams")
-    return jsonify({'img_url':img_url}), 200
-
-
-@app.route('/get-cloud-iac', methods=['POST'])
-def cloud_iac_code_gen():
-    data = request.get_json()
-    prompt = data.get('prompt')
-
-    if not prompt:
-        return jsonify({'error': 'Statement is missing'}), 400
-
-    # Return the query result as JSON
-    result = return_iac_code(prompt)
-    return jsonify({'yaml-code':result}), 200
-
-@app.route('/get-complete-cloud-architecture', methods=['POST'])
-def cloud_complete_archi_generate():
-    data = request.get_json()
-    prompt = data.get('bizProb') + ". " + ". ".join(data.get('answers'))
-
-    if not prompt:
-        return jsonify({'error': 'Statement is missing'}), 400
-
-    # Return the query result as JSON
-    result = get_cloud_architecture(prompt)
-    return jsonify(result), 200
-
-@app.route('/generate-questions', methods=['POST'])
+@app.route('/llm', methods=['POST'])
 def questions_gen():
     data = request.get_json()
     prompt = data.get('bizProb')
@@ -86,37 +48,9 @@ def questions_gen():
         return jsonify({'error': 'Statement is missing'}), 400
 
     # Return the query result as JSON
-    result = return_questions(prompt)
+    result = openai_complete(prompt)
     return json.loads(result), 200
 
-
-@app.route('/iterate-architecture', methods=['POST'])
-def iterate_archi():
-    data = request.get_json()
-    bizProb = data.get('bizProb')
-    recentArchitecture = data.get('recentArchitecture')
-    constraint = data.get('constraint')
-
-    # if not bizProb or not recentArchitecture or not constraint:
-    #     return jsonify({'error': 'Statement is missing'}), 400
-
-    # Return the query result as JSON
-    result = iterate_cloudplan(bizProb, json.dumps(recentArchitecture), constraint)
-    return jsonify({'data':result}), 200
-
-@app.route('/calculate-cost', methods=['POST'])
-def calculate_cost():
-    data = request.get_json()
-    bizProb = data.get('bizProb')
-    recentArchitecture = data.get('recentArchitecture')
-    chatHistory = data.get('chatHistory')
-
-    # if not bizProb or not recentArchitecture or not constraint:
-    #     return jsonify({'error': 'Statement is missing'}), 400
-
-    # Return the query result as JSON
-    result = calculate_cost_cloudplan(bizProb, json.dumps(recentArchitecture),chatHistory)
-    return json.loads(result), 200
 
 if __name__ == '__main__':
     app.run(debug=True, use_reloader=True)
--- scripts/get_rag_answer.py
+++ scripts/get_rag_answer.py
@@ -35,44 +35,25 @@ load_dotenv()
 MAX_HISTORY_LENGTH = 6
 
 
-def transform_json_string(json_string):
-    data = json.loads(json_string)
+# def transform_json_string(json_string):
+#     data = json.loads(json_string)
 
-    transformed_data = {
-        "title": data["title"],
-        "introduction": data["introduction"],
-        "layers": []
-    }
+#     transformed_data = {
+#         "answer": data["answer"]
+#     }
 
-    for layer in data["layers"]:
-        transformed_layer = {
-            "title": layer["title"],
-            "services": layer["services"],
-            "purpose": layer["purpose"],
-            "key_features": []
-        }
+#     # Assuming you have source document information in the original JSON
+#     for source_doc in data("source_documents"):
+#         transformed_source_doc = {
+#             "source": source_doc["source"],
+#             "title": source_doc["title"],
+#             "excerpt": source_doc["excerpt"]
+#         }
+#         transformed_feature["sourceDocuments"].append(transformed_source_doc)
 
-        for feature in layer["key_features"]:
-            transformed_feature = {
-                "feature": feature["feature"],
-                "explanation": feature["explanation"],
-                "sourceDocuments": []
-            }
-            
-            # Assuming you have source document information in the original JSON
-            for source_doc in feature.get("sourceDocuments", []):
-                transformed_source_doc = {
-                    "source": source_doc["source"],
-                    "title": source_doc["title"],
-                    "excerpt": source_doc["excerpt"]
-                }
-                transformed_feature["sourceDocuments"].append(transformed_source_doc)
+#     transformed_layer["key_features"].append(transformed_feature)
 
-            transformed_layer["key_features"].append(transformed_feature)
-
-        transformed_data["layers"].append(transformed_layer)
-
-    return transformed_data
+#     return transformed_data
 
 
 
@@ -100,12 +81,23 @@ def build_chain():
         return_source_documents=True
     )
 
-    prompt_template = """I am providing business requirements - {question}. I want you to provide me with a technical cloud architecture solution document that details usage of which AWS cloud services to use, architecture, performance, speed and other important metrics for security, reliability,. From whatever limited information is given, your task is to suggest the best possible system design document for cloud infrastructure by leveraging cloud services. Feel free to choose any service from AWS. The answer should be at least 1500 words long with suitable headings and subheadings. The response should be STRICTLY in the following JSON format.{{title:string of the project, introduction:string layers:[{{title:title string of layer, services: services used for this layer[], purpose:string, key_features:[{{ feature:string, explanation:string which explains the feature in about 50 words, sourceDocuments:[{{ source:string URL which is the metadata source of the feature, title: string - title of the source document, excerpt: string - excerpt of the source document }}] }}],}}] - where each source document has a unique title, document, string,summary:string which has to be technically sound}}. Here's some background context for your reference {context}. Give the solution in the requested JSON format:"""
+    prompt_template = """
+        You are a chatbot answering questions over enterprise data. Here's the question you have been asked - {question}. From whatever limited information is given, your task is to retrieve the relevant documents and generate an answer. The response should be STRICTLY in the following JSON format.
+            {{
+                answer: answer string,
+                source_documents:[{{ source:string URL which is the metadata source of the feature, title: string - title of the source document, excerpt: string - excerpt of the source document }}] - where each source document has a unique title, document, string,summary:string which has to be technically sound
+            }}. 
+        
+        Here's some background context for your reference {context}. Give the solution in the requested JSON format:"""
     PROMPT = PromptTemplate(
         template=prompt_template, input_variables=["context", "question"]
     )
 
-    return ConversationalRetrievalChain.from_llm(llm=llm,  retriever=retriever, condense_question_prompt=PROMPT, return_source_documents=True)
+    return ConversationalRetrievalChain.from_llm(
+        llm=llm,  
+        retriever=retriever, 
+        condense_question_prompt=PROMPT, return_source_documents=True
+    )
 
 
 def run_chain(chain, prompt: str, history=[]):
@@ -114,7 +106,7 @@ def run_chain(chain, prompt: str, history=[]):
     return result
 
 
-def get_cloud_architecture(query):
+def get_rag_answer(query):
     qa = build_chain()
     chat_history = []
     if (query.strip().lower().startswith("new search:")):
@@ -134,6 +126,7 @@ def get_cloud_architecture(query):
                 "excerpt": metadata["excerpt"]
             }
             source_docs.append(json_document)
-    struc_answer = transform_json_string(restructure_answer(result['answer']))
-    return {'data':struc_answer, 'source_docs': source_docs}
+    
+    # struc_answer = transform_json_string(restructure_answer(result['answer']))
+    return {'data':result['answer'], 'source_docs': source_docs}
 
