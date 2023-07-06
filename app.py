d8ca3a1a2412d4e828ccafb124a0e7cf9f338b81
Ankit Sanghvi <ankit.sanghavi87@gmail.com>
2023-07-06 11:21:37 +0530

    trying to debug pinecone

--- app.py
+++ app.py
@@ -1,11 +1,21 @@
-from flask import Flask, request, jsonify
-import openai
+# External modules
 import os
 import json
+import openai
 from flask_cors import CORS
+from dotenv import load_dotenv
+from flask import Flask, request, jsonify
+from werkzeug.utils import secure_filename
+from langchain.document_loaders import PyPDFLoader, UnstructuredImageLoader, UnstructuredPowerPointLoader
+from langchain.embeddings.openai import OpenAIEmbeddings
+from langchain.text_splitter import CharacterTextSplitter
+from langchain.vectorstores import Pinecone
+import pinecone
+
+# Custom modules
 from scripts.get_rag_answer import get_rag_answer
 from scripts.openai_complete import openai_complete
-from dotenv import load_dotenv
+
 
 load_dotenv()
 
@@ -50,6 +60,44 @@ def questions_gen():
     # Return the query result as JSON
     result = openai_complete(prompt)
     return json.loads(result), 200
+
+@app.route('/custom-index', methods=['POST'])
+def process_file():
+    # Save the uploaded file
+    file = request.files['file']
+    file_type = request.form['file_type']
+    file_name = secure_filename(file.filename)
+    file.save(os.path.join(app.config['UPLOAD_FOLDER'], file_name))
+
+    # Determine the file loader based on file type
+    if file_type == '.pdf':
+        loader = PyPDFLoader(file_path=file_name)
+    elif file_type == '.jpg':
+        loader = UnstructuredImageLoader(file_path=file_name)
+    elif file_type == '.ppt':
+        loader = UnstructuredPowerPointLoader(file_path=file_name)
+    else:
+        return 'Unsupported file type', 400
+
+    documents = loader.load()
+    text_splitter = CharacterTextSplitter(chunk_size=2048, chunk_overlap=0)
+    docs = text_splitter.split_documents(documents)
+
+    embeddings = OpenAIEmbeddings()
+
+    # initialize pinecone
+    pinecone.init(
+        api_key=os.environ.get("PINECONE_API_KEY"),
+        environment="asia-southeast1-gcp-free"
+)
+    index_name = "chat-verlab"
+    # pinecone_index = pinecone.Index(index_name=index_name)
+
+    # print("Creating 🍍 index...")
+    Pinecone.from_documents(docs, embeddings, index_name=index_name)
+
+    return 'File successfully processed and indexed', 200
+
 
 
 if __name__ == '__main__':
